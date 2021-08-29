ca6ea22a39506a798a47e69afccbffe1b4af2539
Abdul-Aziz8 <abdulazizdinho@gmail.com>
2021-08-29 16:11:27 -0700

    Puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,4 @@
 #!/bin/bash
-# include <stdio.h>
-# include <string.h>
-int main()
-{
-  char string[40]
-    strcpy(str, "Programming is like building a multilingual puzzle,\n");
-  puts(string);
-  return 0;
-}
+#include<stdio.h> int main() { char str[] ="Programming is like building a multilingual puzzle,\n" ;  puts(str) return 0;  }
+
+
