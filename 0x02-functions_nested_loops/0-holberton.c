90e9a9ffbaca140abad4994d3e8f89b958abb564
Abdul-Aziz8 <abdulazizdinho@gmail.com>
2021-08-31 13:34:11 -0700

    Print commit

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Putchar (Holberton)
+ * followed by a new line.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar("\"Holberton);
+	return (0);
+}
