71a1c86f4ad47a8b3cc3cdc8a4c730353884e65d
Abdul-Aziz8 <abdulazizdinho@gmail.com>
2021-09-15 05:53:50 -0700

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * prints new string folloed by a new line
+ *@*s: is a pointer to a char
+ *
+ * return: a string
+ */
+void _puts_recursion(char *s)
+{
+	char s[20] = "Hello";
+
+	printf("%s\n", s);
+	return (0);
+}
