687a26317c659098474d767435ace3571a97ba7f
Abdul-Aziz8 <abdulazizdinho@gmail.com>
2021-09-20 23:58:01 -0700

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,10 +10,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	for (i = 0; i < argc; i++)
-	{
-		printf("argv[%d]: %s\n", i, argv[i]);
-	}
+       printf("%s\n", argv[0]);
+       if (argc < 2)
+       {
+	      printf("\n");
+       }
+       else
+       {
+	       printf("%s\n", argv[1]);
+       }
 	return (0);
 }
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: the number of command line arguments
+ * @argv: an array containing the program command line argument
+ *
+ * Return: Success.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	printf("\n");
+	return (0);
+}
