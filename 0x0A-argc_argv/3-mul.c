49c32b636d13fcf60d1001544f93a91ed81e5b48
Abdul-Aziz8 <abdulazizdinho@gmail.com>
2021-09-21 16:17:40 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,6 +12,7 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2, product;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
@@ -19,7 +20,7 @@ int main(int argc, char *argv[])
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-	product = num1 *num2;
+	product = num1 * num2;
 	printf("%d\n", product);
 	return (0);
 }
