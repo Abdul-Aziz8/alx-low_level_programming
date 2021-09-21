bb7ba90b6222a516c6fd4aa2587ad68bfc2aa002
Abdul-Aziz8 <abdulazizdinho@gmail.com>
2021-09-21 16:09:02 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints multiples of two numbers
+ * @argc: number of command line arguments
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: Success.
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, product;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	product = num1 *num2;
+	printf("%d\n", product);
+	return (0);
+}
+
